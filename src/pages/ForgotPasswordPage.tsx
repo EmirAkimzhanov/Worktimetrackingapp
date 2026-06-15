f7dbd76d95002546a851bf37881bbbe82ef60c0a
Emirbek Akimzhanov <emirbek.akimzhanov@mazars.kg>
2026-06-15 17:45:03 +0600

    .

--- src/pages/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
-import { useForgotPassword } from '../hooks/UseAuth';
+import { useForgotPassword } from '../hooks/useAuth';
 
 const LoginContainer = styled.div`
   min-height: 100vh;
